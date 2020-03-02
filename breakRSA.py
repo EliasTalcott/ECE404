d8f25e59a394ef60ccece192a4ddb1a1147ce65a
Eli Talcott <eli.talcott@gmail.com>
2020-03-02 10:08:43 -0500

    Initial Solution Files

---
+++ breakRSA.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+# Homework Number: 6
+# Name: Elias Talcott
+# ECN Login: etalcott
+# Due Date: March 3, 2020
+
+from BitVector import *
+
+###
+## Call syntax:
+## python3 rsa.py -g p.txt q.txt
+## python3 rsa.py -e message.txt p.txt q.txt encrypted.txt
+## python3 rsa.py -d encrypted.txt p.txt q.txt decrypted.txt
+###
+
+if __name__ == "__main__":
+    pass
---
+++ rsa.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+# Homework Number: 6
+# Name: Elias Talcott
+# ECN Login: etalcott
+# Due Date: March 3, 2020
+
+from BitVector import *
+
+###
+## Call syntax:
+## python3 breakRSA.py -e message.txt enc1.txt enc2.txt enc3.txt n_1_2_3.txt #Steps 1 and 2
+## python3 breakRSA.py -c enc1.txt enc2.txt enc3.txt n_1_2_3.txt cracked.txt #Step 3
+###
+
+if __name__ == "__main__":
+    pass
